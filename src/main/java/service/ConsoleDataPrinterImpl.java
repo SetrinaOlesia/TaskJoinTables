19c221a2d68e894b8bc66ff96003bbf393c10290
Setrina Olesia <setrina527@gmail.com>
2021-04-25 18:04:00 +0300

    fix ConsoleDataPrinterImpl

--- src/main/java/service/ConsoleDataPrinterImpl.java
+++ src/main/java/service/ConsoleDataPrinterImpl.java
@@ -8,25 +8,33 @@ import java.util.List;
 
 public class ConsoleDataPrinterImpl implements ConsoleDataPrinter {
 
+    final String employeesFilePath = "src/main/resources/file/Test Task.csv";
+    final String departmentsFilePath = "src/main/resources/file/Test_Task.csv";
+
+    FileReader fileReader = new FileReaderImpl();
+    FileParser fileParser = new FileParserImpl();
+
     @Override
     public void showDataInConsole() throws ReaderException {
-        FileReader fileReader = new FileReaderImpl();
-        FileParser fileParser = new FileParserImpl();
-
-        String employeesFilePath = "src/main/resources/file/Test Task.csv";
-        String departmentsFilePath = "src/main/resources/file/Test_Task.csv";
-
-        List<String> employeesFileLines = fileReader.readFile(employeesFilePath);
-        List<String> departmentsFileLines = fileReader.readFile(departmentsFilePath);
-
-        List<Employee> employeeList = fileParser.parseEmployeesCsv(employeesFileLines);
-        List<Department> departmentLis = fileParser.parseDepartmentsCsv(departmentsFileLines);
+        List<Employee> employeeList = this.getEmployeesList();
+        List<Department> departmentList = this.getDepartmentsList();
         for (Employee employee : employeeList) {
-            for (Department department : departmentLis) {
+            for (Department department : departmentList) {
                 if (employee.getDepartmentId().equals(department.getId())) {
                     System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getSurname() + " " + department.getName());
                 }
             }
         }
     }
+
+    private List<Employee> getEmployeesList() throws ReaderException {
+        List<String> employeesFileLines = fileReader.readFile(employeesFilePath);
+        return fileParser.parseEmployeesCsv(employeesFileLines);
+    }
+
+    private List<Department> getDepartmentsList() throws ReaderException {
+        List<String> departmentsFileLines = fileReader.readFile(departmentsFilePath);
+        return fileParser.parseDepartmentsCsv(departmentsFileLines);
+    }
+
 }
